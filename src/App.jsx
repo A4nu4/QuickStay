6c1990ee2592eac868b57973c5e611b506c144e0
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-26 12:14:10 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Routes, useLocation, Route } from "react-router-dom";
+
+function App() {
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
