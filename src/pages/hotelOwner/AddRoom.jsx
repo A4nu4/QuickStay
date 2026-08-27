9869c38eb4fe23e12e877786274cd22d76bde97c
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-08-27 16:31:38 +0500

    Fixed bug with Amenities in AddRoom

--- src/pages/hotelOwner/AddRoom.jsx
+++ src/pages/hotelOwner/AddRoom.jsx
@@ -12,7 +12,7 @@ const AddRoom = () => {
   const [inputs, setInputs] = React.useState({
     roomType: "",
     pricePerNight: 0,
-    amenitites: {
+    amenities: {
       "Free WiFi": false,
       "Free Breakfast": false,
       "Room Service": false,
@@ -90,23 +90,23 @@ const AddRoom = () => {
         </div>
         <p className="text-gray-800 mt-4">Amenities</p>
         <div className="flex flex-col flex-wrap mt-1 text-gray-400 max-w-sm">
-          {Object.keys(inputs.amenitites).map((amenity, index) => (
+          {Object.keys(inputs.amenities).map((amenity, index) => (
             <div key={index}>
               <input
                 type="checkbox"
                 id={`amenities${index + 1}`}
-                checked={inputs.amenitites[amenity]}
+                checked={inputs.amenities[amenity]}
                 onChange={() =>
                   setInputs({
                     ...inputs,
                     amenities: {
-                      ...inputs.amenitites,
+                      ...inputs.amenities,
                       [amenity]: !inputs.amenities[amenity],
                     },
                   })
                 }
               />
-              <label htmlFor={`amenities${index + 1}`}>{amenity}</label>
+              <label htmlFor={`amenities${index + 1}`}> {amenity}</label>
             </div>
           ))}
         </div>
